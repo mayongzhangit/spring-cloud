040ea2cee3e5b77eca48ad75f4c54a62df2d73dc
yzMa <2641007740@qq.com>
2020-08-05 20:00:06 +0800

    没必要的zipkin server

--- discovery-server/src/main/java/com/myz/discovery/server/DiscoveryStartApp.java
+++ discovery-server/src/main/java/com/myz/discovery/server/DiscoveryStartApp.java
@@ -1,10 +1,20 @@
 package com.myz.discovery.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @author yzMa
  * @desc
  * @date 2020/8/5 14:06
  * @email 2641007740@qq.com
  */
+@EnableEurekaServer
+@SpringBootApplication
 public class DiscoveryStartApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryStartApp.class,args);
+    }
 }
